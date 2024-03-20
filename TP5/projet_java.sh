04be83d4b94afc2a4058f4d1fbc3328c6e4bc255
Julien Bailly <julien.bailly2@getu.univ-orleans.fr>
2024-03-20 11:30:12 +0100

    bash rapide

---
+++ TP5/projet_java.sh
@@ -0,0 +1,3 @@
+javac -d ./bin ./src/*.java
+#javadoc -d "./doc" "$repertoire_source"/*.java
+java -cp ./bin Executable
--- TP5/src/UIWeekEnd.java
+++ TP5/src/UIWeekEnd.java
@@ -2,6 +2,36 @@ class UIWeekEnd {
     public static void main(String[] args) {
 	WeekEnd leWeekEnd = new WeekEnd();
 	AppWeekEnd app = new AppWeekEnd(leWeekEnd);
+	Personne pierre = new Personne("Durand","Pierre"); 
+	System.out.println(pierre.getNom()); 
+	Personne paul = new Personne("Dupond","Paul"); 
+	Personne marie = new Personne("Dumond","Marie");
+	Personne anne = new Personne("Dunon","Anne");  
+	Depense d1 = new Depense(pierre, 12, "pain"); 	
+	Depense d2 = new Depense(paul, 100, "pizza"); 
+	Depense d3 = new Depense(pierre, 70, "essence");
+	Depense d4 = new Depense(marie, 15, "vin");  
+	Depense d5 = new Depense(paul, 10, "vin"); 
+	WeekEnd we = new WeekEnd();  
+	we.ajouterPersonne(pierre); 
+	we.ajouterPersonne(paul);
+	we.ajouterPersonne(marie);
+	we.ajouterPersonne(anne); 
+	we.ajouterDepense(d1);   
+	we.ajouterDepense(d2); 
+	we.ajouterDepense(d4);   
+	we.ajouterDepense(d3);
+	we.ajouterDepense(d5);
+	System.out.println("total des dépenses "+we.totalDepenses()) ;
+	System.out.println("moyenne des dépenses "+we.depensesMoyenne()) ;
+	System.out.println("total des dépenses de Pierre "+ we.totalDepensesPersonne(pierre)) ;
+	System.out.println("total des dépenses de Paul "+ we.totalDepensesPersonne(paul)) ;
+	System.out.println("total des dépenses de Marie "+ we.totalDepensesPersonne(marie)) ;
+	System.out.println("total des dépenses de Anne "+ we.totalDepensesPersonne(anne)) ;
+	System.out.println("depenses vin "+we.depenseProduit("vin"));
+	System.out.println("avoir de pierre "+we.avoirPersonne(pierre));
+	System.out.println("avoir de paul "+we.avoirPersonne(paul));	
+	System.out.println("avoir de marie "+we.avoirPersonne(marie));
 	app.run();
     }
 }
@@ -28,14 +58,38 @@ class AppWeekEnd {
     public void menu() {
 	boolean commande_faite = false;
 	while(!commande_faite) {
-	    System.out.println("Que voulez vous faire?");
-	    System.out.println("Q: quitter");
+		System.out.println("╭─────────────────────────────────────╮");
+	    System.out.println("│ Que voulez vous faire?              │");
+		System.out.println("│─────────────────────────────────────│");
+	    System.out.println("│ Q: quitter                          │");
+		System.out.println("│ P: gens week-end                    │");
+		System.out.println("│ D: dépense week-end                 │");
+		System.out.println("│ T: total dépense week-end           │");
+		System.out.println("│ M: dépense moyenne par gens         │");
+		System.out.println("╰─────────────────────────────────────╯");
 	    String commande_brute = System.console().readLine();
 	    String commande = commande_brute.strip().toLowerCase();
 	    if(commande.equals("q")) {
 		quitter = true;
 		commande_faite = true;
-	    } else {
+	    } 
+		else if(commande.equals("p")){
+			System.out.println(we.toString());
+            commande_faite = true;
+		}
+		else if(commande.equals("d")){
+			System.out.println(we.toString());
+            commande_faite = true;
+		}
+		else if(commande.equals("t")){
+			System.out.println(we.toString());
+            commande_faite = true;
+		}
+		else if(commande.equals("m")){
+			System.out.println(we.toString());
+            commande_faite = true;
+		}
+		else {
 		System.out.println("Commande '" + commande_brute + "' invalide.");
 	    }
 	}
@@ -50,7 +104,9 @@ class AppWeekEnd {
 
     /// Affiche un message d'au revoir
     public void au_revoir() {
-	
+	System.out.println("╭──────────────────────────────────────────╮");
+	System.out.println("│ Au revoir mon quoikoubaka à la cerise !! │");
+	System.out.println("╰──────────────────────────────────────────╯");
     }
 
 }
